244771b00c0a271e8eaa0b3ac397423c76274376
Guyde-dev <nikko.c.turner@msd549c.org>
2024-12-16 12:36:37 -0800

    update

--- JS Scripts/economics.js
+++ JS Scripts/economics.js
@@ -3,6 +3,6 @@ function evolve() {
     alert("Your community has evolved to the economic phase.\nEconomics Tab Unlocked.")
     tabButtons[3].hidden = false
 }
-sleep(1000).then(() => {
+sleep(3000).then(() => {
     evolve()
 })
