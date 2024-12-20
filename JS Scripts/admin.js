16388268ee2992d7972cf4d6462f6678be89a8c1
Guyde-dev <nikko.c.turner@msd549c.org>
2024-12-20 14:50:30 -0800

    update

--- JS Scripts/admin.js
+++ JS Scripts/admin.js
@@ -1,28 +1,25 @@
 var admin = document.getElementById("admin")
-var confirm = document.getElementById("confirm")
 
 function adminLogin() {
-    if (admin.value == atob("SUtwM0JaZEJhQU1pS1ZPVzJMMGk3ZVdKMQo=")) {
-        lumberjacks = 10000
-        stokers = 10000
-        farmers = 10000
-        miners = 10000
-        builders = 10000
-        population = 10000
-        maxpopulation = 1000000000
+    if (admin.value == atob("SUtwM0JaZEJhQU1pS1ZPVzJMMGk3ZVdKMQ==")) {
+        lumberjacks = 100000
+        stokers = 100000
+        farmers = 100000
+        miners = 100000
+        builders = 100000
+        population = 100000
+        maxpopulation = 10000000000
         firehealth = 100
-        wood = 10000
-        huts = 10000
+        wood = 100000
+        huts = 100000
         unlocked = true
         unlocked1 = true
-        perodons = 1000000000
-        ironore = 10000
-        iron = 10000
-        gold = 10000
-        goldore = 10000
-        food = 10000
+        perodons = 100000
+        ironore = 100000
+        iron = 100000
+        gold = 100000
+        goldore = 100000
+        food = 10000000
         saveData()
-        loadData()
-        admin.value = ""
     }
-})
+}
